659658707f2e00d9953e629d859af08e926b3586
Ковальчук Евгений Васильевич <joli.88@mail.ru>
2021-11-28 14:04:48 +0300

    Лабораторная работа 2. Первые 3 пункта

---
+++ Tasks/Task 2/3.sql
@@ -0,0 +1,9 @@
+--3. Для каждой профессии найти суммарное количество человек, 
+--которые были приняты позже 2010 года, из таблицы HumanResources.Employee, 
+--не учитывать пустые значения.
+SELECT JobTitle
+     , COUNT(*)
+FROM HumanResources.Employee
+WHERE HireDate IS NOT NULL
+GROUP BY JobTitle
+HAVING MAX(JobTitle) > '2010'
---
+++ Tasks/Task 2/4.sql
@@ -0,0 +1,5 @@
+--4)Вывести типы (PhoneNumberTypeID) из таблицы Person.PersonPhone, 
+--где в номере телефона данного типа первые три цифры совпадают с последними тремя
+--и количество таких номеров телефонов больше 5.
+SELECT PhoneNumberTypeID
+FROM Person.PersonPhone
