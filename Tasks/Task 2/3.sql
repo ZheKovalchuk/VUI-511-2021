37b938986aa075b491628a217875c5dde5328bc8
Ковальчук Евгений Васильевич <joli.88@mail.ru>
2021-12-05 17:24:09 +0300

    Итог

--- Tasks/Task 2/3.sql
+++ Tasks/Task 2/3.sql
@@ -1,8 +1,7 @@
---3. Для каждой профессии найти суммарное количество человек, 
+--3) Для каждой профессии найти суммарное количество человек, 
 --которые были приняты позже 2010 года, из таблицы HumanResources.Employee, 
 --не учитывать пустые значения.
-SELECT JobTitle
-     , COUNT(*)
+SELECT JobTitle, COUNT(*)
 FROM HumanResources.Employee
 WHERE HireDate IS NOT NULL
 GROUP BY JobTitle
